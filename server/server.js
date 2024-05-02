b99026a7bfdbf6cc954cc49b078c61de49d30dfd
FLopriore <115494520+FLopriore@users.noreply.github.com>
2024-05-02 16:11:26 +0200

    Connect the server to the DB

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 import express from "express";
 import cors from "cors";
-import records from "./routes/record.js";
+import mongoose from "mongoose";
+//import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 3000;
+const uri = process.env.ATLAS_URI || "";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/record", records);
+//app.use("/record", records);
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+mongoose.connect(uri)
+.then(()=> {
+  console.log("Connected to database");
+
+  // start the Express server
+  app.listen(PORT, ()=> {
+      console.log(`Server listening on port ${PORT}`);
+  });
+})
+.catch((e) => {
+  console.log(e);
+})
