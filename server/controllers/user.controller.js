8d0c950bc8a513f36a4630684a52b8ef5db9d457
FLopriore <115494520+FLopriore@users.noreply.github.com>
2024-05-04 13:17:10 +0200

    Set saveUninitialized to true

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,21 +2,25 @@ const User = require("../models/user.model.js");
 const passport = require("passport")
 
 //TODO: Specify and else
-const CreateUser = async (req,res,next) =>{
+const createUser = async (req, res, next) => {
     const newUser = new User({email: req.body.email}); //Crea l'user e gli chiede come param l'email
     User.register(newUser, req.body.password, (error, user) => {
-        if (user) next();
-        });
+        if (user) {
+            next();
+            // add redirect to login page
+        }
+        // else ADD ERROR MESSAGE
+    });
 };
 
-const AuthUser = passport.authenticate("local",
-{
-    failureRedirect: "/user/login", //Route Lato Client
-    failireFlash: "Login non riuscito",
-    successRedirect:"/", //Route lato Client
-    successFlash:"Login non riuscito"
-}
+const authUser = passport.authenticate("local",
+    {
+        failureRedirect: "/user/login", //Route Lato Client
+        failureFlash: "Login non riuscito",
+        successRedirect: "/", //Route lato Client
+        successFlash: "Login riuscito"
+    }
 );
 
 
-module.exports = {CreateUser,AuthUser};
+module.exports = {createUser, authUser};
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
     {
         email: {
             type: String,
             unique: true
-            },
-}
+        },
+    }
 );
 //Aggiunge automaticamente il field password allo schema
-userSchema.plugin(passportLocalMongoose,{usernameField: "email"}); //Specifica che il campo username è quello dello Schema
+userSchema.plugin(passportLocalMongoose, {usernameField: "email"}); //Specifica che il campo username è quello dello Schema
 
-module.exports = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
+module.exports = User;
