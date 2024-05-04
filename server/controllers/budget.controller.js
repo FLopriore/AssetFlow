80bf57a80857295299458c37ef2dc03893824452
FLopriore <115494520+FLopriore@users.noreply.github.com>
2024-05-04 11:11:09 +0200

    Add 404 error to Budget entries

--- server/controllers/budget.controller.js
+++ server/controllers/budget.controller.js
@@ -1,13 +1,16 @@
 const Income = require("../models/income.model.js");
 const Expense = require("../models/expense.model.js");
 
-// TODO: check if the promises work well, especially 'res.status(200).json(responses)'
 const getBudgetEntries = (req, res) => {
     const incomesPromise = Income.find({}).exec();  // exec() is necessary to get promises from queries
     const expensePromise = Expense.find({}).exec();
 
     Promise.all([incomesPromise, expensePromise])
-        .then( (responses) => {
+        .then((responses) => {
+            // Check if both income and expense responses are empty
+            if (!responses[0].length && !!responses[1].length) {
+                return res.status(404).json({message: "No budget entries found!"});
+            }
             res.status(200).json(responses);
         })
         .catch((e) => res.status(500).json({message: e.message}));
