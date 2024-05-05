9d262523e83848dae6da35320f75e845622a16b7
FrancescoDesiderato <desiderato.francescosalvatore@gmail.com>
2024-05-05 12:40:44 +0200

    Include Middleware

---
+++ server/middleware/verifyToken.js
@@ -0,0 +1,31 @@
+const jsonwebtoken = require("jsonwebtoken");
+const User = require("../models/user.model.js");
+
+const SECRET_KEY = process.env.SECRET_KEY || "";
+
+function verifyToken(req,res,next){
+    let token = req.headers.token;
+    if (!token) return res.status(401).json({error:true,message:"Provide Token"});
+    else{
+        const decoded = jsonwebtoken.verify(token,SECRET_KEY,
+         (errors,payload) => {
+            if(payload){
+                //Verificato il token dovrebbe restituirmi l'ID dell'utente
+                User.findById(payload.data).then(user => {
+                    if(user){next()
+                    }else{res.status(httpStatus.FORBIDDEN).json({
+                        error: true,
+                        message: "No User account found."
+                        });}
+                }
+            );
+            } else {
+                return res.status(httpStatus.UNAUTHORIZED).json({
+                    error:"true",
+                    message: "Invalid Token"
+                });
+            }
+         }
+        );
+    }
+}
