e6a8d0494fbaae3d46a9cd3aca66dcb45c6bd844
shira0614 <shira>
2024-05-08 12:22:09 +0200

    added graphs in asset

--- client/src/components/Asset.jsx
+++ client/src/components/Asset.jsx
@@ -1,13 +1,59 @@
 import * as React from 'react';
 import Sidebar from './Sidebar';
-import { Box } from '@mui/material';
+import { Box, Button, CardContent, Grid, Card } from '@mui/material';
+import BasicLineChart from './LineChart';
 
 export default function Expense() {
     return (
-        <Box className='outerbox'>
+        <Box className='window'>
             <Sidebar />
-            <Box className='innerbox'>
-                    <p>Questo è un placeholder per la sezione "Trading"</p>
+            <Box className='main-content'>
+                <Grid container display='flex' flexDirection='row' alignItems='stretch'>
+                    <Grid item xs={12}>
+                        <Box sx={{display: 'flex', 
+                        flexDirection: 'row', 
+                        gap: '1rem',
+                        mt: '1rem',
+                        ml: '2rem'
+                        }}>
+                            <Button variant='outlined'>
+                                Portafogli
+                            </Button>
+                            <Button variant='outlined'>
+                                Watchlist
+                            </Button>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Box sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}>
+                            <BasicLineChart />
+                            <Card sx={{
+                                display: 'flex',
+                                width: '100%',
+                                height: '100%',
+                                margin: '1rem'
+                            }}>
+                                <Box sx={{width: '100%', display: 'flex', 
+                                flexDirection: 'column', alignItems: 'flex-center', 
+                                bgcolor: '#dbdbdb'}}>
+                                    <CardContent>
+                                        <p>Notizie</p>
+                                    </CardContent>
+                                </Box>
+                            </Card>
+                        </Box>
+                        
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Box>
+                        <p>assets</p>
+                        </Box>
+                        
+                    </Grid>
+                </Grid>
             </Box>
         </Box>
     );
