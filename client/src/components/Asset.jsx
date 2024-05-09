209d7cd0a73bea1abbc008084cb627142cb4e347
shira0614 <shira>
2024-05-09 12:30:07 +0200

    completed asset section graphics

--- client/src/components/Asset.jsx
+++ client/src/components/Asset.jsx
@@ -1,56 +1,88 @@
 import * as React from 'react';
 import Sidebar from './Sidebar';
-import { Box, Button, CardContent, Grid, Card } from '@mui/material';
+import { Box, Fab, Grid, List, ListItem } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ListItemText from '@mui/material/ListItemText';
+import FolderIcon from '@mui/icons-material/Folder';
+import DeleteIcon from '@mui/icons-material/Delete';
 import BasicLineChart from './LineChart';
+import AddIcon from '@mui/icons-material/Add';
+import { useState, useEffect } from "react";
 
-export default function Expense() {
+
+function generate(element) {
+    return [0, 1, 2].map((value) =>
+      React.cloneElement(element, {
+        key: value,
+      }),
+    );
+  }
+
+export default function Asset() {
+
+    const [assetList, setAssetList] = useState([])
+
     return (
         <Box className='window'>
             <Sidebar />
             <Box className='main-content'>
                 <Grid container display='flex' flexDirection='row' alignItems='stretch'>
                     <Grid item xs={12}>
-                        <Box sx={{display: 'flex', 
-                        flexDirection: 'row', 
-                        gap: '1rem',
-                        mt: '1rem',
-                        ml: '2rem'
+                        <Box sx={{ 
+                            width: '100%',
+                            textAlign: 'center',
+                            mt: '1rem',
+                            mb: '4rem'
                         }}>
-                            <Button variant='outlined'>
-                                Portafogli
-                            </Button>
-                            <Button variant='outlined'>
-                                Watchlist
-                            </Button>
+                            <h2>Nome Asset</h2>
                         </Box>
                     </Grid>
                     <Grid item xs={6}>
+                       <BasicLineChart /> 
+                    </Grid>
+                    <Grid item xs={6} display='flex' flexWrap='wrap' justifyContent='center'>
                         <Box sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
+                            width: '80%',
+                            height: '100%',
+                            bgcolor: '#eaeaea',
+                            borderRadius: '20px',
+                            alignSelf: 'center',
+                            textAlign: 'center',
+                            padding: '0.5rem',
+                            overflowY: 'scroll'
                         }}>
-                            <BasicLineChart />
-                            <Card sx={{
-                                display: 'flex',
-                                width: '100%',
-                                height: '100%',
-                                margin: '1rem'
+                            <h3>I tuoi asset</h3>
+                            <List>
+                            {generate(
+                <ListItem
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemAvatar>
+                    <Avatar>
+                      <FolderIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Single-line item"
+                  />
+                </ListItem>,
+              )}
+                            </List>
+
+                            <Fab color='primary' sx={{
+                                position: 'absolute',
+                                top: '71%',
+                                left: '91%'
                             }}>
-                                <Box sx={{width: '100%', display: 'flex', 
-                                flexDirection: 'column', alignItems: 'flex-center', 
-                                bgcolor: '#dbdbdb'}}>
-                                    <CardContent>
-                                        <p>Notizie</p>
-                                    </CardContent>
-                                </Box>
-                            </Card>
+                            <AddIcon />
+                            </Fab>
                         </Box>
-                        
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Card>
-                            
-                        </Card>
                     </Grid>
                 </Grid>
             </Box>
