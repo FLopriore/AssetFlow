516128cfc250ef1a3e8ef83d1b70f50c24501cd9
FLopriore <115494520+FLopriore@users.noreply.github.com>
2024-05-21 12:06:30 +0200

    Remove a TODO

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const {createUser, authUser} = require("../controllers/user.controller.js");
 
-// TODO: delete account (and logout maybe?)
 router.post("/signup", createUser);
 router.post("/login", authUser);
 
