3d8961a9282e5ab370808b0fe5797cc911406a9f
FLopriore <115494520+FLopriore@users.noreply.github.com>
2024-05-21 12:30:18 +0200

    Fix token verification

--- server/routes/verify.route.js
+++ server/routes/verify.route.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const verify = require("../controllers/verify.controller.js");
+const verifyToken = require('../middleware/verifyToken.js');
+
+router.use(verifyToken);
 
 router.get("/",verify);
 
